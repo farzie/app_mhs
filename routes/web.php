a9410fd1b94f6df112f0812bbe75cb2af3756c8b
farzie <farizexemail@gmail.com>
2025-09-28 14:38:37 +0700

    ui init

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file defines all web routes for your application.
+|
+*/
+
+// Home page route (the authentication gate)
+Route::get('/', [AuthController::class, 'home'])->name('home');
+
+// Authentication handler routes
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
